4059da58f5f826a3747877c00d496310060bd838
JoaquimGCVS <jgui9292@gmail.com>
2025-11-15 02:29:56 -0300

    Estilizando componentes de busca e exibicao de clima atual

--- src/components/ClimaAtual.jsx
+++ src/components/ClimaAtual.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { formatarDataHora } from '../utils/formatador';
 import styles from './ClimaAtual.module.css';
+import { FaTemperatureArrowDown, FaTemperatureArrowUp } from "react-icons/fa6";
+import { FaWind } from "react-icons/fa";
+import { FiSunrise, FiSunset } from "react-icons/fi";
+import { IoWaterOutline } from "react-icons/io5";
+import { RiWaterPercentFill } from "react-icons/ri";
 
 const ClimaAtual = ({ dadosClima }) => {
-    
-    const { 
-        name, 
+
+    const {
+        name,
         sys: { country, sunrise, sunset },
         main: { temp, temp_max, temp_min, humidity },
-        weather, 
+        weather,
         wind: { speed },
-        dt, 
-        timezone 
+        dt,
+        timezone
     } = dadosClima;
 
     const iconeUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
     const descricaoClima = weather[0].description;
-    
+
     const dataHoraFormatada = formatarDataHora(dt, timezone);
     const nascerDoSolFormatado = formatarDataHora(sunrise, timezone).substring(17, 22);
     const porDoSolFormatado = formatarDataHora(sunset, timezone).substring(17, 22);
@@ -24,52 +29,51 @@ const ClimaAtual = ({ dadosClima }) => {
 
     return (
         <div className={styles.climaAtual}>
-            
-            <div className={styles.cabecalho}>
-                <h2>{name}, {country}</h2>
-                <p>Última Medição: {dataHoraFormatada}</p>
-            </div>
 
             <div className={styles.condicaoPrincipal}>
-                <img src={iconeUrl} alt={descricaoClima} />
-                <div className={styles.textoEsquerda}>
-                    <h1 className={styles.temperatura}>{Math.round(temp)}°C</h1>
-                    <p className={styles.descricao}>{descricaoClima}</p>
-                </div>
+                <p className={styles.descricao}>{descricaoClima}</p>
             </div>
 
-            <div className={styles.detalhes}>
-                
-                <div className={styles.detalheItem}>
-                    <p>Mínima</p>
-                    <p className={styles.valorDetalhe}>{Math.round(temp_min)}°C</p>
+            <div className={styles.condicaoDetalhada}>
+                <h1 className={styles.temperatura}>{Math.round(temp)}°</h1>
+                <div>
+                    <div className={styles.cabecalho}>
+                        <h2>{name}, {country}</h2>
+                        <img src={iconeUrl} alt={descricaoClima} />
+                    </div>
+
+                    <div className={styles.detalhes}>
+                        <div className={styles.detalheItem}>
+                            <FaTemperatureArrowDown />
+                            <p className={styles.valorDetalhe}>{Math.round(temp_min)}°C</p>
+                        </div>
+
+                        <div className={styles.detalheItem}>
+                            <FaTemperatureArrowUp />
+                            <p className={styles.valorDetalhe}>{Math.round(temp_max)}°C</p>
+                        </div>
+
+                        <div className={styles.detalheItem}>
+                            <RiWaterPercentFill />
+                            <p className={styles.valorDetalhe}>{humidity}%</p>
+                        </div>
+
+                        <div className={styles.detalheItem}>
+                            <FaWind />
+                            <p className={styles.valorDetalhe}>{velocidadeVentoKmh} km/h</p>
+                        </div>
+
+                        <div className={styles.detalheItem}>
+                            <FiSunrise />
+                            <p className={styles.valorDetalhe}>{nascerDoSolFormatado}</p>
+                        </div>
+
+                        <div className={styles.detalheItem}>
+                            <FiSunset />
+                            <p className={styles.valorDetalhe}>{porDoSolFormatado}</p>
+                        </div>
+                    </div>
                 </div>
-                
-                <div className={styles.detalheItem}>
-                    <p>Máxima</p>
-                    <p className={styles.valorDetalhe}>{Math.round(temp_max)}°C</p>
-                </div>
-                
-                <div className={styles.detalheItem}>
-                    <p>Umidade</p>
-                    <p className={styles.valorDetalhe}>{humidity}%</p>
-                </div>
-                
-                <div className={styles.detalheItem}>
-                    <p>Vento</p>
-                    <p className={styles.valorDetalhe}>{velocidadeVentoKmh} km/h</p>
-                </div>
-                
-                <div className={styles.detalheItem}>
-                    <p>Nascer do Sol</p>
-                    <p className={styles.valorDetalhe}>{nascerDoSolFormatado}</p>
-                </div>
-                
-                <div className={styles.detalheItem}>
-                    <p>Pôr do Sol</p>
-                    <p className={styles.valorDetalhe}>{porDoSolFormatado}</p>
-                </div>
-                
             </div>
 
         </div>
